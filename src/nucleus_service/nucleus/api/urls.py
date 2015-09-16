b5f270336d3005a9787860ffafcf9205da6f438a
Dmitry Mishin <dmishin@sdsc.edu>
2015-09-16 12:56:24 -0700

    Oops, forgot comma

--- src/nucleus_service/nucleus/api/urls.py
+++ src/nucleus_service/nucleus/api/urls.py
@@ -3,6 +3,6 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns(
     'api.views',
     url(r'^cluster$', 'cluster_list', name='cluster_list'),
-    url(r'^cluster/(?P<cluster_name>\w+)/stop$', 'cluster_stop', name='cluster_stop')
+    url(r'^cluster/(?P<cluster_name>\w+)/stop$', 'cluster_stop', name='cluster_stop'),
     url(r'^cluster/(?P<cluster_name>\w+)/start$', 'cluster_start', name='cluster_start')
 )
