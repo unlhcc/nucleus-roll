a821d034f312679a9af457b5c8db65b5cd47f786
Gregor von Laszewski <laszewski@gmail.com>
2015-09-16 15:57:59 -0400

    add class view

--- src/nucleus_service/nucleus/api/views.py
+++ src/nucleus_service/nucleus/api/views.py
@@ -2,7 +2,28 @@ from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-@api_view(['GET'])
-def cluster_list(request):
-    return Response("response")
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
 
+# @api_view(['GET'])
+# def cluster_list(request):
+#    return Response("response")
+
+class Cluster(APIView):
+    """
+    List all clusters, or create a new cluster.
+    """
+    def get(self, request, format=None):
+        #clusters = Cluster.objects.all()
+        #serializer = CLusterSerializer(clusters, many=True)
+        #return Response(serializer.data)
+        return Response("todo")
+        
+    def post(self, request, format=None):
+        #serializer = ClusterSerializer(data=request.data)
+        #if serializer.is_valid():
+        #    serializer.save()
+        #    return Response(serializer.data, status=status.HTTP_201_CREATED)
+        #return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        return Response("todo")
