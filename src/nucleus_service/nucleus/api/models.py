19a74699fd28c89b397ddf6433ac7dd865132bfa
Gregor von Laszewski <laszewski@gmail.com>
2015-09-16 17:27:30 -0400

    adding first template for models

--- src/nucleus_service/nucleus/api/models.py
+++ src/nucleus_service/nucleus/api/models.py
@@ -1,3 +1,55 @@
 from django.db import models
 
-# Create your models here.
+"""
+class User(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    firstname = models.CharField(max_length=100)
+    lastname = models.CharField(max_length=100)
+    username = models.CharField(max_length=24)
+    email = models.CharField(max_length=100)            
+
+    class Meta:
+        ordering = ('name',)
+        
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('username', 'firstname', 'lastname', 'email', 'created')
+        
+class Project(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('name')
+                
+class Cluster(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+
+class ClusterSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('name')
+        
+                
+class Storagepool(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        ordering = ('name',)
+        
+class StoragepoolSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Snippet
+        fields = ('name')
+"""
