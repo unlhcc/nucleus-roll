9a84101d5a7706308e1635886d3da01d35a25310
Gregor von Laszewski <laszewski@gmail.com>
2015-09-21 15:12:17 -0400

    simplify parameters

--- src/nucleus_service/nucleus/command/cluster.py
+++ src/nucleus_service/nucleus/command/cluster.py
@@ -1,7 +1,7 @@
 """Usage:
       vcluster info [--user=USER]
                     [--project=PROJECT]
-      vcluster list [--name=NAME]
+      vcluster list [--name=NAMES]
                     [--user=USER]
                     [--project=PROJECT]
                     [--hosts=HOSTS]
@@ -12,41 +12,42 @@
       vcluster delete [all]
                       [--user=USER]
                       [--project=PROJECT]
-                      [--name=NAME]
+                      [--name=NAMES]
                       [--hosts=HOSTS]
                       [--start=TIME_START]
                       [--end=TIME_END]
                       [--host=HOST]
       vcluster delete --file=FILE
-      vcluster update [--name=NAME]
+      vcluster update [--name=NAMES]
                       [--hosts=HOSTS]
                       [--start=TIME_START]
                       [--end=TIME_END]
       vcluster add [--user=USER]
                    [--project=PROJECT]
-                   [--hosts=HOSTS]
+                   [--host=HOST]
                    [--description=DESCRIPTION]
-                   --name=NAMES
-                   --start=TIME_START
-                   --end=TIME_END
+                   [--start=TIME_START]
+                   [--end=TIME_END]
+                   NAME
       vcluster add --file=FILE
 
-    Options:
-      --name=NAMEs          Names of the vcluster
-      --user=USER           user name
-      --project=PROJECT     project id
-      --start=TIME_START    Start time of the vcluster, in
-                            YYYY/MM/DD HH:MM:SS format. [default: 1901-01-01]
-      --end=TIME_END        End time of the vcluster, in
-                            YYYY/MM/DD HH:MM:SS format. In addition a duratio
-                            can be specified if the + sign is the first sig
-                            The duration will than be added to
-                            the start time. [default: 2100-12-31]
-      --host=HOST           host name
-      --description=DESCRIPTION  description summary of the vcluster
-      --file=FILE           Adding multiple vclusters from one file
-      --format=FORMAT       Format is either table, json, yaml or csv
-                            [default: table]
+      Options:
+        --user=USER           user name
+        --name=NAMES          Names of the vcluster
+        --start=TIME_START    Start time of the vcluster, in
+			                  YYYY/MM/DD HH:MM:SS format.
+                              [default: 1901-01-01]
+        --end=TIME_END  End time of the vcluster, in YYYY/MM/DD
+                        HH:MM:SS format. In addition a duratio can be
+                        specified if the + sign is the first sig The
+                        duration will than be added to the start
+                        time. [default: 2100-12-31] 
+        --project=PROJECT     project id
+        --host=HOST           host name
+        --description=DESCRIPTION  description summary of the vcluster
+        --file=FILE           Adding multiple vclusters from one file
+        --format=FORMAT       Format is either table, json, yaml or csv
+                              [default: table]
    Description:
        vcluster info
           lists the resources that support vcluster for
@@ -58,5 +59,5 @@ from docopt import docopt
 
 
 if __name__ == '__main__':
-    arguments = docopt(__doc__, version='0.1.1rc')
+    arguments = docopt(__doc__, version='1.0.1')
     print(arguments)
