f54994b943b0c8353f3a898b69833a1c93cbff70
Dmitry Mishin <dmishin@sdsc.edu>
2016-03-15 15:55:19 -0700

    MySQL setup WORKSgit add .nucleus-roll.metadata nodes/nucleus-service.xml src/anyjson/ src/billiard/ src/celery/ src/kombu/ src/uwsgi/! WOOHOOOgit add .nucleus-roll.metadata nodes/nucleus-service.xml src/anyjson/ src/billiard/ src/celery/ src/kombu/ src/uwsgi/git add .nucleus-roll.metadata nodes/nucleus-service.xml src/anyjson/ src/billiard/ src/celery/ src/kombu/ src/uwsgi/ Also added python libs, and using nginx temporary

---
+++ src/celery/version.mk
@@ -0,0 +1,16 @@
+NAME            = celery
+VERSION         = 3.1.23
+RELEASE 	= 0
+
+SOURCE_DIR	= $(NAME)-$(VERSION)
+
+SRC_SUBDIR         = $(NAME)
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS           = $(SOURCE_PKG)
+
