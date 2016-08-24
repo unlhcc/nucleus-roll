5449110f2169c6745d408a12da2cc95bf391caff
Trevor Cooper <tcooper@sdsc.edu>
2016-08-24 12:14:23 -0700

    Fix RPM release tagging in build of nucleus-guacamole-client RPM
    Resolves CMT-721

--- src/nucleus-guacamole-client/version.mk
+++ src/nucleus-guacamole-client/version.mk
@@ -1,7 +1,8 @@
 NAME            = nucleus-guacamole-client
 VERSION         = 0.9.8
-RELEASE 	= 34eddef1c5
+BUILD_HASH 	= 34eddef
+RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
-SOURCE_DIR		= $(NAME)
+SOURCE_DIR	= $(NAME)
 
 GIT_REPO	= ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
