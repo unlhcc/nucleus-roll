d35566c2260fe771f6c811a34a4181aad98e5441
Trevor Cooper <tcooper@sdsc.edu>
2016-08-24 12:18:10 -0700

    Fix RPM release tagging for nucleus-service RPM
    Resolves CMT-722

--- src/nucleus-service/version.mk
+++ src/nucleus-service/version.mk
@@ -1,7 +1,8 @@
 NAME            = nucleus-service
 VERSION         = 1.0
-RELEASE 	= 1a8ad0c61
+BUILD_HASH 	= 1a8ad0c
+RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
-SOURCE_DIR		= $(NAME)
+SOURCE_DIR	= $(NAME)
 
 GIT_REPO	= ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
