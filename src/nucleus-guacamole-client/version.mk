8dbc26d6d7efb80b3c8306e831080c87810cced4
Trevor Cooper <tcooper@sdsc.edu>
2016-08-24 17:30:42 -0700

    Set BUILD_HASHES for nucleus-service and nucleus-guacamole-client

--- src/nucleus-guacamole-client/version.mk
+++ src/nucleus-guacamole-client/version.mk
@@ -1,8 +1,8 @@
 NAME            = nucleus-guacamole-client
 VERSION         = 0.9.8
-BUILD_HASH 	= 34eddef
+BUILD_HASH      = 34eddef
 RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
-SOURCE_DIR	= $(NAME)
+SOURCE_DIR      = $(NAME)
 
-GIT_REPO	= ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
+GIT_REPO        = ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
--- src/nucleus-service/version.mk
+++ src/nucleus-service/version.mk
@@ -1,8 +1,8 @@
 NAME            = nucleus-service
 VERSION         = 1.0
-BUILD_HASH 	= 1a8ad0c
+BUILD_HASH      = 48423f7
 RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
-SOURCE_DIR	= $(NAME)
+SOURCE_DIR      = $(NAME)
 
-GIT_REPO	= ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
+GIT_REPO        = ssh://git@forge.sdsc.edu:7999/xsd/$(NAME).git
