0b8c72d902a6ab40c4e333592e1d100082efd3c1
Dmitry Mishin <dmitry.mishin@gmail.com>
2017-02-07 15:20:51 -0800

    Several issues fixed

--- src/nucleus-service/version.mk
+++ src/nucleus-service/version.mk
@@ -1,6 +1,6 @@
 NAME            = nucleus-service
 VERSION         = 1.0
-BUILD_HASH      = 0be92f1
+BUILD_HASH      = cdef25b
 RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
 SOURCE_DIR      = $(NAME)
