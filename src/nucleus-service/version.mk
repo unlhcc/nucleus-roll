ecf05ab401bbf4d7bec03d660424323d1d6bf5c7
Dmitry Mishin <dmitry.mishin@gmail.com>
2017-08-30 10:38:21 -0700

    Fix for rabbitmq retries

--- src/nucleus-service/version.mk
+++ src/nucleus-service/version.mk
@@ -1,6 +1,6 @@
 NAME            = nucleus-service
 VERSION         = 1.0
-BUILD_HASH      = 24a71a1
+BUILD_HASH      = e7bd28a
 RELEASE        :=$(shell bash ../../version.sh -r).g$(BUILD_HASH)
 
 SOURCE_DIR      = $(NAME)
